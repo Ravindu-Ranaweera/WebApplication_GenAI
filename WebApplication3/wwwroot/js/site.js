d4bbbdf97c83b04010c92e9fc3d5dbef9e3ba9eb
Ravindu-Ranaweera <inforanaweera@gmail.com>
2023-10-13 13:33:04 +0530

    Home

--- WebApplication3/wwwroot/js/site.js
+++ WebApplication3/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const cardSections = document.querySelectorAll('.card-section');
+
+window.addEventListener('scroll', () => {
+    for (const cardSection of cardSections) {
+        const boundingClientRect = cardSection.getBoundingClientRect();
+
+        if (boundingClientRect.top >= 0 && boundingClientRect.bottom <= window.innerHeight) {
+            cardSection.classList.add('in-viewport');
+        } else {
+            cardSection.classList.remove('in-viewport');
+        }
+    }
+});
